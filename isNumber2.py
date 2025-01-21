71e2e03f0ec7bd8ce57a32f07dcb7f9229f07022
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 22:45:34 +0600

    Add print statement for regex groups in isNumber2.py

--- isNumber2.py
+++ isNumber2.py
@@ -6,3 +6,4 @@ print('Phone number: ' + MatchObject.group())
 print('Country Code: ' + MatchObject.group(1)) #Group(1) means first group in the regex
 print('Main Number: ' + MatchObject.group(2)) #Group(2) means second group in the regex
 print('Main Number with country code: ' + MatchObject.group(0)) #Group(0) means entire match
+print(MatchObject.groups()) # groupS() returns a tuple of multiple values

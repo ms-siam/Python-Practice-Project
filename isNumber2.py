e187a6d7b4a91014718f7ff5f58f2dd05c916aeb
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 23:06:32 +0600

    Enhance phone number regex to make country code optional and update print statements for clarity

--- isNumber2.py
+++ isNumber2.py
@@ -1,8 +1,10 @@
 import re
 
-phoneNumRegex = re.compile(r'(\+\d{3}) (\d{4}-\d{6})')
+phoneNumRegex = re.compile(r'(\+\d{3})? (\d{4}-\d{6})') #added ? after (\+\d{3}) to make it optional
 MatchObject = phoneNumRegex.search('My number is +880 1521-768750')
+MatchObject1 = phoneNumRegex.search('My number is 1521-768750')
 print('Phone number: ' + MatchObject.group())
+print('Phone number(countrycode optional): ' + MatchObject1.group())
 print('Country Code: ' + MatchObject.group(1)) #Group(1) means first group in the regex
 print('Main Number: ' + MatchObject.group(2)) #Group(2) means second group in the regex
 print('Main Number with country code: ' + MatchObject.group(0)) #Group(0) means entire match

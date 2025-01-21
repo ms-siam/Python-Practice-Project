a207a8273e11e93c9676324ef89601bfc1960893
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 22:42:38 +0600

    Enhance phone number regex in isNumber2.py to capture country code and main number separately

--- isNumber2.py
+++ isNumber2.py
@@ -1,5 +1,8 @@
 import re
 
-phoneNumRegex = re.compile(r'\+\d{3} \d{4}-\d{6}')
+phoneNumRegex = re.compile(r'(\+\d{3}) (\d{4}-\d{6})')
 MatchObject = phoneNumRegex.search('My number is +880 1521-768750')
 print('Phone number: ' + MatchObject.group())
+print('Country Code: ' + MatchObject.group(1)) #Group(1) means first group in the regex
+print('Main Number: ' + MatchObject.group(2)) #Group(2) means second group in the regex
+print('Main Number with country code: ' + MatchObject.group(0)) #Group(0) means entire match

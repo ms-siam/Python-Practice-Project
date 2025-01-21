d6ed1aa545b9e8a45a4f3a7160710c29914e9512
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 23:03:06 +0600

    Add regex matching for Batwoman and optional Batman variations in practise.py

--- practise.py
+++ practise.py
@@ -4,3 +4,9 @@ batRegex = re.compile(r'Bat(man|mobile|copter|bat)') #instead of (r'Batman | Bat
 matchObject = batRegex.search('Batman lost his bat while driving Batmobile')
 print(matchObject.group())
 print(matchObject.group(1)) #group(1) will return the first group of the matched string
+
+batRegex1 = re.compile(r'Bat(wo)?man') # ? means optional 
+matchObject1 = batRegex1.search('The adventures of Batman')  
+print(matchObject1.group()) # Batman
+matchObject2 = batRegex1.search('The adventures of Batwoman')
+print(matchObject2.group()) # Batwoman

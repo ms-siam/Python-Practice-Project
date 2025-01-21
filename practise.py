30afcca2fa851224c2d8ac0d9a48974af508dd93
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 22:59:25 +0600

    Refactor regex in practise.py to match variations of Batman and extract groups

--- practise.py
+++ practise.py
@@ -1,7 +1,6 @@
 import re
-heroRegex = re.compile(r'Batman|Spider man') # | is used for or , and called pipe( | )
-matchObject1 = heroRegex.search('Batman and Spider man')# it will return the first occurance of the pattern
-print(matchObject1.group()) #Batman
 
-matchObject2 = heroRegex.search('Spider man and Batman')
-print(matchObject2.group()) #Spider man
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)') #instead of (r'Batman | Batmobile | Batcopter | Batbat')
+matchObject = batRegex.search('Batman lost his bat while driving Batmobile')
+print(matchObject.group())
+print(matchObject.group(1)) #group(1) will return the first group of the matched string

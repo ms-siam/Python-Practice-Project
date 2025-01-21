5e19c40dcd3aafdae1f00f50e97d03ae054baa0f
ms-siam <mobaroksiam10@gmail.com>
2025-01-21 22:22:50 +0600

    Uncomment code to print name and total sum in fiveTimes.py

--- fiveTimes.py
+++ fiveTimes.py
@@ -1,10 +1,10 @@
-#print('My name is')
-#for i in range(5):
-   # print('Jimmy five times (' + str(i) + ')')
-#total = 0
-#for num in range(10):
-   # total = total + num
-#print(total)
+print('My name is')
+for i in range(5):
+    print('Jimmy five times (' + str(i) + ')')
+total = 0
+for num in range(10):
+    total = total + num
+print(total)
 print('My name is ')
 i = 0
 while i < 5:

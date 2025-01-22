3a7aa80b70bf0017d8e91efb946183cedb4a06d5
ms-siam <mobaroksiam10@gmail.com>
2025-01-22 07:14:43 +0600

    Improve readability of yourName2.py by adding comments to clarify while loop functionality

--- yourName2.py
+++ yourName2.py
@@ -4,3 +4,5 @@ while True:
     if name == 'your name':
         break
 print('Thank You')
+
+# The code is shorter and more readable. The while loop runs forever until a break statement is executed    

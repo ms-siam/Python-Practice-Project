87487f5bc33e08ba0196979ef1be237fe07062f6
ms-siam <mobaroksiam10@gmail.com>
2025-01-22 14:15:49 +0600

    Refactor date regex pattern in findDate.py for improved accuracy and readability

--- findDate.py
+++ findDate.py
@@ -1,42 +1,30 @@
 import re
 
 dateRegex = re.compile(r'''(
-                       (\d\d)
-                       (\s\-\.\/)
-                       (\d\d)
-                       (\s\-\.\/)
-                       (\d{4})
+                       (\d{2})[-\/\.](\d{2})[-\/\.](\d{4})
                        |
-                       (\w+)
-                       (\s)
-                       (\d{1,2})
-                       (\s\,\.\/)
-                       (\d{4})
+                       (\w+)\s(\d{1,2})[,\s\.](d{4})
                        |
-                       (\d{1,2})
-                       (\s)
-                       (\w+)
-                       (\s\,\.\/)
-                       (\d{4})
+                       (\d{1,2})\s(\w+)[,\s](\d{4})
                        )''', re.VERBOSE | re.IGNORECASE)
 
-text = "Today's date is 01-01-2020. Tomorrow's date is 02-01-2020. Yesterday's date was 31-12-2019."
+text = "Today's date is 22-01-2025. Tomorrow's date is 23-01-2025. Yesterday's date was 21-01-2025."
 
 matches = dateRegex.findall(text)
 
 dates = []
 
 for match in matches:
-    if match[1] and match[3] and match[5]:
-        day, month, year = match[1], match[3], match[5] 
+    if match[1] and match[2] and match[3]:
+        day, month, year = match[1], match[2], match[3] 
         dates.append(f'{day}-{month}-{year}')
     
-    elif match[6] and match[8] and match[10]:
-        month, day, year = match[6], match[8], match[10]
+    elif match[4] and match[5] and match[6]:
+        month, day, year = match[4], match[5], match[6]
         dates.append(f'{month} {day}, {year}')  
     
-    elif match[11] and match[13] and match[15]:
-        day, month, year = match[11], match[13], match[15]
+    elif match[7] and match[8] and match[9]:
+        day, month, year = match[7], match[8], match[9]
         dates.append(f'{day} {month}, {year}')
 
 if dates:

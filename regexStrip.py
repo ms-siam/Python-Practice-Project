28f1d1f59fabbf35c50e68c4a7d71693c1880d3b
ms-siam <mobaroksiam10@gmail.com>
2025-01-23 11:18:19 +0600

    Refactor regex_strip function to simplify character stripping logic and improve readability

--- regexStrip.py
+++ regexStrip.py
@@ -1,23 +1,18 @@
 import re
-def regex_strip(string, chars):
+def regex_strip(string, chars=None):
     if chars:
         chars = re.escape(chars)
         strip = re.compile(rf'''(
                            ^[{chars}]+|[{chars}]+$
                            )''', re.VERBOSE)
         matches = strip.findall(string)
-        for match in matches:
-            string = re.sub(rf'{match}', '',string)
-        return string
+        string = strip.sub('', string)
     else:
         strip = re.compile(r'''(
                            ^\s*|\s*$
                            )''', re.VERBOSE)
         matches = strip.findall(string)
-        for match in matches:
-            string = re.sub(rf'{match}', '',string)
-        return string
-    
-
+        string = strip.sub('', string)
+    return string
 text = input("Enter a text: ")
-print(f"{regex_strip(text, 'a')}")
+print(f"{regex_strip(text, 's')}")

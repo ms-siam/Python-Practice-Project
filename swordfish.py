9fe570d4a9abbc5aa120bcbc1c01bbad1dcd1ec3
ms-siam <mobaroksiam10@gmail.com>
2025-01-24 10:11:24 +0600

    Add comment to clarify loop behavior in swordfish.py

--- swordfish.py
+++ swordfish.py
@@ -1,10 +1,12 @@
+
 while True:
     print('What is your name?')
     name = input()
     if name != 'Siam':
-        continue
+        continue                            # Jump into First line of while loop
     print('Hello, Siam. What is the password? (It  is a fish)')
     password = input()
     if password == 'Swordfish':
         break
 print('Access Granted')
+

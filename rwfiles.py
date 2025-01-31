d9abe81f55071fadb6d37f5e39e6ea39be70f8eb
ms-siam <mobaroksiam10@gmail.com>
2025-01-31 16:32:38 +0600

    Add example to calculate total size of files in a directory in rwfiles.py

--- rwfiles.py
+++ rwfiles.py
@@ -50,4 +50,9 @@ print(os.path.split(calcFilePath))
 print(calcFilePath.split(os.sep))
 
 print(os.path.getsize('C:\\Windows\\System32\\calc.exe'))
-print(os.listdir('C:\\Windows\\System32'))
+#print(os.listdir('C:\\Windows\\System32'))
+
+totalSize = 0
+for filename in os.listdir('C:\\Windows\\System32'):
+    totalSize = totalSize + os.path.getsize(os.path.join('C:\\Windows\\System32', filename))
+print(totalSize)

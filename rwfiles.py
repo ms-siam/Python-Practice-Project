a2caa867c80dc370bcb888cdc7560f4e493f7b7a
ms-siam <mobaroksiam10@gmail.com>
2025-01-31 11:11:32 +0600

    Add functionality to join filenames from a list to a folder path in rwfiles.py

--- rwfiles.py
+++ rwfiles.py
@@ -2,3 +2,9 @@
 from pathlib import Path
 print(Path('spam', 'bacon', 'eggs'))
 print(str(Path('spam', 'bacon', 'eggs')))
+
+#Join names from a list of filenames to end of a folder's name:
+myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
+for filename in myFiles:
+    print(Path('C:\\Users\\Mobarok', filename))
+    

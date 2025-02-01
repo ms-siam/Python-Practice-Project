131e581ee248226fcf167ebd6f7e211e7de1fec9
ms-siam <mobaroksiam10@gmail.com>
2025-02-01 11:16:10 +0600

    Update madLibs.py to write a default template to MadLibs.txt

--- madLibs.py
+++ madLibs.py
@@ -1,5 +1,6 @@
 
 import re
 
-with open('MadLibs.txt', 'r') as file:
+with open('MadLibs.txt', 'w') as file:
+    file.write('The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.')
     

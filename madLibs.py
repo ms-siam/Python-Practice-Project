bab630334bca7cea8aec5d70da80aa96473baa1e
ms-siam <mobaroksiam10@gmail.com>
2025-02-01 12:11:31 +0600

    Enhance madLibs.py to allow user input for adjectives in the template

--- madLibs.py
+++ madLibs.py
@@ -3,4 +3,10 @@ import re
 
 with open('MadLibs.txt', 'w') as file:
     file.write('The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.')
+
+words = file.split()
+for word in words:
+    if word == 'ADJECTIVE':
+        adjective = input('Enter an adjective: ')
+        words[word] = adjective
     

31a78cb25548ebe24e129bbbe3d2f9d47f1ec85f
ms-siam <mobaroksiam10@gmail.com>
2025-02-01 12:14:44 +0600

    Refactor madLibs.py to read from MadLibs.txt and process user inputs for adjectives, nouns, and verbs

--- madLibs.py
+++ madLibs.py
@@ -3,17 +3,18 @@ import re
 
 with open('MadLibs.txt', 'w') as file:
     file.write('The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.')
-
-words = file.split()
-for word in words:
-    if word == 'ADJECTIVE':
-        adjective = input('Enter an adjective: ')
-        words[word] = adjective
-    elif word == 'NOUN':
-        noun = input('Enter a noun: ')
-        words[word] = noun
-    elif word == 'VERB':
-        verb = input('Enter a verb: ')
-        words[word] = verb
-Story = ' '.join(words)
-print(Story)
+with open('MadLibs.txt', 'r') as file:
+    file = file.read()
+    words = file.split()
+    for word in words:
+        if word == 'ADJECTIVE':
+            adjective = input('Enter an adjective: ')
+            words[word] = adjective
+        elif word == 'NOUN':
+            noun = input('Enter a noun: ')
+            words[word] = noun
+        elif word == 'VERB':
+            verb = input('Enter a verb: ')
+            words[word] = verb
+    Story = ' '.join(words)
+    print(Story)

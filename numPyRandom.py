c143980f57d9bd03a417b7c4cc64f74ea3726839
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 02:59:48 +0600

    Refactor seaborn displot calls for clarity in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -178,6 +178,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.displot(random.uniform(low=3, high=5, size = 1000))
-sns.displot(random.uniform(size = 1000))
-plt.show()
+sns.displot(random.uniform(low=3, high=5, size = 1000))   #From 3 to 5 
+sns.displot(random.uniform(size = 1000))  #From 0 to 1

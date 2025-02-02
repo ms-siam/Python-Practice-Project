326f10fb34a1e4a4b15ca245e0c129d2f67cd7fd
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 02:53:23 +0600

    Add uniform distribution sample generation in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -167,3 +167,7 @@ plt.legend()  #It shows the label in plot
 plt.show()'''
 
 #Uniform Distribution
+#Create a 2x3 uniform distribution sample:
+from numpy import random
+x=random.uniform(size=(2,3))
+print(x)

a5c02a5f325814b3528c0d3922edbd9fd96357b0
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 01:29:08 +0600

    Replace histplot with displot for binomial distribution visualization in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -111,5 +111,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.histplot(random.binomial(n=10, p=0.5, size=1000))
+sns.displot(random.binomial(n=10, p=0.5, size=1000))
 plt.show()

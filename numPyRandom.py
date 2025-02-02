5958ba11943e98c72767ec48d17c2d8e428b4fa3
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 01:26:19 +0600

    Remove KDE from binomial distribution visualization in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -111,5 +111,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, kde=True)
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True)
 plt.show()

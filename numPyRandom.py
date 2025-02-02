a625acae9eb4e06f7f5a1cd5f8100056d457914e
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 02:06:59 +0600

    Add legend to binomial and normal distribution visualization

--- numPyRandom.py
+++ numPyRandom.py
@@ -121,4 +121,5 @@ import seaborn as sns
 
 sns.kdeplot(random.normal(loc=50,scale=5, size=1000), label='normal')
 sns.kdeplot(random.binomial(n=100, p=0.5, size=1000), label='binomial')
+plt.legend()
 plt.show()

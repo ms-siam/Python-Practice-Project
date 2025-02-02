684e0dea2324a5b8a43450fcc0f2beadaa641e51
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 00:48:13 +0600

    Add generation of random normal distribution with specified mean and standard deviation in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -79,3 +79,10 @@ from numpy import random
 
 x = random.normal(size=(2, 3))
 print(x)
+
+#Generate a random normal distribution of size 2x3 with mean at 1 and standard deviation of 2
+
+from numpy import random
+
+xwls = random.normal(loc= 1, scale= 2, size= (2,3))
+print(x)

d752babb97f24008fcb26cf605d344128904dbb4
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 01:24:29 +0600

    Add visualization for binomial distribution using seaborn in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -104,3 +104,12 @@ from numpy import random
 x = random.binomial(n=10, p=0.5, size=10)
 
 print(x)
+
+#Visualization of binomial Distribution
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, KDE=False)
+plt.show()

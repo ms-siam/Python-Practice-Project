0a42f8e0ace644913ffca08048dfdf6c4bb3f20a
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 01:25:48 +0600

    Fix parameter name for KDE in binomial distribution visualization in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -111,5 +111,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, KDE=False)
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, kde=False)
 plt.show()

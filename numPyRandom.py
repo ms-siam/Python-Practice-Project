4cd908e081fb324b14d8a8caafa106d913e06df1
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 00:32:38 +0600

    Replace sns.histplot with sns.displot for non-histogram plotting in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -70,5 +70,5 @@ sns.histplot([0, 1, 2, 3, 4, 5])
 plt.show()
 
 #Plotting a distplot without the histogram
-sns.histplot([0, 1, 2, 3, 4, 5], hist=False)
+sns.displot([0, 1, 2, 3, 4, 5], hist=False)
 plt.show()

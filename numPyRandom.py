8e227c53c4d8837b9912b31fd7e15503dbb89a58
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 00:53:46 +0600

    Add visualization for normal distribution using seaborn in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -88,3 +88,11 @@ xwls = random.normal(loc= 1, scale= 2, size= (2,3))
 print(xwls)
 
 #Visualization of normal distrbution
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.normal(size = 1000), hist = False)
+
+plt.show()

6efeb719631f1946cbf8b68821f000571803e58b
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 02:28:42 +0600

    Add visualization for Poisson distribution using seaborn in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -132,3 +132,12 @@ x = random.poisson(lam=2, size=10)
 print(x)'''
 
 #Visualization of Poison Distribution
+
+from numpy import random
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+sns.displot(random.poisson(lam=2, size=1000))
+plt.show()

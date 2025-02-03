89d3fe6a05f84bb56cc824cd3f1dbaca9aa62908
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 17:06:08 +0600

    Add sample generation for Pareto distribution in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -276,3 +276,8 @@ sns.displot(random.rayleigh(size = 1000), kind="kde")
 plt.show()'''
 
 #Pareto Distribution
+#Draw out a sample for pareto distribution with shape of 2 with size 2x3:
+
+from numpy import random
+x=random.pareto(a = 2, size=(2,3))
+print(x)

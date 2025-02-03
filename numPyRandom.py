8ec9eae93c8c9ce39ab6e6a8fffeeeceecb60d84
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 17:13:48 +0600

    Replace histplot with distplot for Pareto distribution visualization in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -287,5 +287,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.histplot(random.pareto(a=2, size = 1000), kde=False)
+sns.distplot(random.pareto(a=2, size = 1000), kde=False)
 plt.show()

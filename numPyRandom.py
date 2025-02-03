12ca510721794fad29ed4f16d26dc83c570b0c91
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 16:56:46 +0600

    Add KDE visualization for Rayleigh distribution in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -267,3 +267,10 @@ x=random.rayleigh(scale = 2, size=(2,3))
 print(x)
 
 #Visualization of Rayleigh Distribution
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.rayleigh(size = 1000), kind="kde")
+plt.show()

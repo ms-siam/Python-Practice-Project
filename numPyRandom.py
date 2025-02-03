a7561493074b28ed2d6163f1ad8ce45c5f492051
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 17:11:46 +0600

    Disable KDE in Pareto distribution visualization in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -287,5 +287,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.pareto(a=2, size = 1000))
+sns.distplot(random.pareto(a=2, size = 1000), kde=False)
 plt.show()

117f4e13f97b6d9295205bdb3a78861d6221964a
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 18:50:38 +0600

    Fix print statement formatting and add function type check in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -12,7 +12,7 @@ import numpy as np
 x = [1, 2, 3, 4]
 y = [4, 5, 6, 7]
 z = np.add(x, y)
-print(z)'''
+print(z)
 
 #Create your own ufunc for addition
 import numpy as np
@@ -20,4 +20,6 @@ def myadd(x, y):
     return x+y
 myadd = np.frompyfunc(myadd, 2, 1)
 
-print(myadd([11, 12, 13, 14], [22, 23, 24, 25]))
+print(myadd([11, 12, 13, 14], [22, 23, 24, 25]))'''
+
+#Check function type

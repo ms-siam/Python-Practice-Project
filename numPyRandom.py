d9ef216e96acb8b9dd22ca838ec8e1f1dd6179be
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 17:09:57 +0600

    Add KDE visualization for Pareto distribution in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -283,3 +283,9 @@ x=random.pareto(a = 2, size=(2,3))
 print(x)
 
 ##Visualization of Pareto Distribution
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.pareto(a=2, size = 1000))
+plt.show()

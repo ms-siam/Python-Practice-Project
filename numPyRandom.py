d0879527d26c3889ea776be925e5792bd969a4a5
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 16:42:59 +0600

    Update chi-squared distribution visualization to use KDE in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -256,5 +256,5 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.displot(random.chisquare(df =1, size = 1000))
+sns.displot(random.chisquare(df =1, size = 1000), kind="kde")
 plt.show()

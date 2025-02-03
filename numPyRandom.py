4cef45d05f47329cad6aca444a4cd4df901217f7
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 12:09:42 +0600

    Update exponential distribution visualization to use KDE in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -238,6 +238,6 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.displot(random.exponential(size=1000))
+sns.displot(random.exponential(size=1000), kind="kde")
 plt.show()
 

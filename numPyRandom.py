007a355ae540dcc267185ba555243077067c76c4
ms-siam <mobaroksiam10@gmail.com>
2025-02-03 16:42:41 +0600

    Add seaborn visualization for chi-squared distribution in numPyRandom.py

--- numPyRandom.py
+++ numPyRandom.py
@@ -250,3 +250,11 @@ x=random.chisquare(df = 2, size=(2,3))
 print(x)
 
 #Visualization of Chi Square Distribution
+
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.chisquare(df =1, size = 1000))
+plt.show()

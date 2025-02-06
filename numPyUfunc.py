458d4fd8f17ccbbcff0df9cae1e2364667c7eace
ms-siam <mobaroksiam10@gmail.com>
2025-02-06 21:59:04 +0600

    Update arrays for power example in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -81,8 +81,8 @@ print(newarr)
 #Raise the valules in arr1 to the power of values in arr2:
 import numpy as np
 
-arr1 = np.array([10, 20, 30, 40, 50, 60])
-arr2 = np.array([3, 5, 6, 8, 2, 33])
+arr1 = np.array([10, 2, 3, 4, 5, 6])
+arr2 = np.array([3, 5, 6, 8, 2, 3])
 
 newarr = np.power(arr1, arr2)
 

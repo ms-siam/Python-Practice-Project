7b3c79d3cf2aee835584e2215c99363cc7714605
ms-siam <mobaroksiam10@gmail.com>
2025-02-06 13:09:06 +0600

    Add subtraction example using np.subtract in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -43,3 +43,13 @@ arr2 = np.array([20,21,22,23,24,25])
 newarr = np.add(arr1, arr2)
 
 print(newarr)
+
+#Subtract the values in arr1 to the values in arr2:
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.subtract(arr1, arr2)
+
+print(newarr)

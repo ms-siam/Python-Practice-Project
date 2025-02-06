97c9e0787877eea84f3813e7473bda9d7cbc6783
ms-siam <mobaroksiam10@gmail.com>
2025-02-06 12:37:10 +0600

    Add example for np.add usage in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -34,3 +34,12 @@ print(type(np.concatenate))'''
 #Simple ARITHMETIC
 
 #Addition of arrays
+#Add the values in arr1 to the values in arr2:
+
+import numpy as np
+arr1 = np.array([10,11,12,13,14,15])  
+arr2 = np.array([20,21,22,23,24,25])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)

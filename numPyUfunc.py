9615cc830312e79669b9c9be0f55d73308d8575a
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:23:14 +0600

    Add example for ufunc summations in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -224,3 +224,12 @@ result = nplog(arr, bases)
 print(result)'''
 
 #ufunc Summations
+#Add the values in arr1 to the values in arr2:
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+
+newarr = np.add(arr1, arr2)
+print(newarr)

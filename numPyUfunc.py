aaadfae6f5d85e52350764c02e7c16576e9e4980
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:43:05 +0600

    Add example for product of array elements in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -268,3 +268,11 @@ newarr = np.cumsum(arr)
 print(newarr)'''
 
 #ufunc products
+#Find the product of the elements of this array:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+x = np.prod(arr)
+
+print(x)

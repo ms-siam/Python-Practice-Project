1b94fdea42ce9b6aad50b33fbe13364f58499422
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:40:40 +0600

    Fix print statement formatting and add newline at end of file in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -232,7 +232,7 @@ arr1 = np.array([1, 2, 3])
 arr2 = np.array([1, 2, 3])
 
 newarr = np.add(arr1, arr2)
-print(newarr)'''
+print(newarr)
 
 #Sum the values in arr1 to the values in arr2:
 
@@ -265,4 +265,6 @@ arr = np.array([1, 2, 3])
 
 newarr = np.cumsum(arr)
 
-print(newarr)
+print(newarr)'''
+
+#ufunc products

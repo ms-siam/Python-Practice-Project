713356a522a00792d8464398807f01bfe7fc664c
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:30:38 +0600

    Add example for summation over the first axis in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -245,3 +245,12 @@ newarr = np.sum([arr1, arr2])
 print(newarr)
 
 #Summation over an axis
+#Perform summation in the following array over 1st axis:
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+
+newarr = np.sum([arr1, arr2], axis = 1)
+print(newarr)

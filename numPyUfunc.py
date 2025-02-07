e92836fc91bf3008e090c51d8f5c224723d7bc9d
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:25:41 +0600

    Enhance ufunc summations example in numPyUfunc.py with additional summation method

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -225,11 +225,21 @@ print(result)'''
 
 #ufunc Summations
 #Add the values in arr1 to the values in arr2:
-
+'''
 import numpy as np
 
 arr1 = np.array([1, 2, 3])
 arr2 = np.array([1, 2, 3])
 
 newarr = np.add(arr1, arr2)
+print(newarr)'''
+
+#Sum the values in arr1 to the values in arr2:
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([1, 2, 3])
+
+newarr = np.sum([arr1, arr2])
 print(newarr)

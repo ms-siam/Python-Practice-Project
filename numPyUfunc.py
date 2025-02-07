5320e607a79288b1e896cb84e42b57e3b5a6adb4
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 00:43:26 +0600

    Add example for product of two arrays in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -276,3 +276,10 @@ arr = np.array([1, 2, 3, 4])
 x = np.prod(arr)
 
 print(x)
+
+# Find the product of the elements of two arrays:
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([5,6,7,8])
+
+x = np.prod([arr1, arr2])
+print(x)

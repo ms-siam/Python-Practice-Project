5c1da4e770940c39161a426063b6d702f55e10ed
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 13:01:03 +0600

    Add example for calculating cosh values in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -421,3 +421,12 @@ import numpy as np
 x = np.sinh(np.pi/2)
 
 print(x)
+
+# Find cosh values for all of the values in arr:
+import numpy as np
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x = np.cosh(arr)
+
+print(x)        

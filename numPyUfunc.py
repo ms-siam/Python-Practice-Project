15b9ff836782d8013ed5d0fe0c56fcd58735be1e
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 12:32:43 +0600

    Add example for converting radians to degrees in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -377,3 +377,11 @@ import numpy as np
 arr = np.array([90, 180, 270, 360])
 x = np.deg2rad(arr)
 print(x)
+
+#Convert all of the values in following array arr to degrees:
+import numpy as np
+
+arr = np.array([np.pi/2, np.pi, 1.5*np.pi, 2*np.pi])
+x = np.rad2deg(arr)
+
+print(x)

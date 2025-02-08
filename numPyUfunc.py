a89b275f9b3a7c44ba30f48c3ce40b2e93aa599e
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 12:14:21 +0600

    Add example for finding GCD of an array in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -349,3 +349,10 @@ x = np.gcd(num1, num2)
 print(x)
 
 #Finding GCD in arrays
+
+#Find the GCD for all of the numbers in the following array:
+import numpy as np
+arr = np.array([20, 8, 32, 36,16])
+x = np.gcd.reduce(arr)
+
+print(x)

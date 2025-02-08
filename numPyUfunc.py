fd5daeaa626a698c84886d181a42d71fe2083db8
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 13:06:14 +0600

    Add example for calculating arctanh values in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -438,3 +438,11 @@ import numpy as np
 x = np.arcsinh(1.0)
 
 print(x)      
+
+#Find the angle for all of the tanh values in the array
+import numpy as np
+
+arr = np.array([0.1, 0.2, 0.5])
+x = np.arctanh(arr)
+
+print(x)      

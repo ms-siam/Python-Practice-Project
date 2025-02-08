f7d8e435336d8a9bcffc2b07967c6735e73d6f98
ms-siam <mobaroksiam10@gmail.com>
2025-02-08 12:53:18 +0600

    Add hypotenuse calculation example in numPyUfunc.py

--- numPyUfunc.py
+++ numPyUfunc.py
@@ -403,4 +403,12 @@ x = np.arcsin(arr)
 print(x)     
 
 #Hypotenues
- 
+ # Find the hypotenuse for 4 base and 3 perpendicular:
+import numpy as np
+
+base = 3
+perp = 4
+
+x = np.hypot(base, perp)
+
+print(x)

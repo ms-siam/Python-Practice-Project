07c44d667e36d58e89bc53c870ca6f2feaad64c4
ms-siam <mobaroksiam10@gmail.com>
2025-02-11 02:46:08 +0600

    Raising Exception for lower height

--- boxPrint.py
+++ boxPrint.py
@@ -3,3 +3,5 @@ def boxPrint(symbol, width, height):
         raise Exception('Symbol must be a single character string.')
     if width <= 2:
         raise Exception('Width must be greater than 2.')
+    if height <= 2:
+        raise Exception('Height must be greater than 2.')

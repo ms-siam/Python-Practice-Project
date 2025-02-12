b6eed107432ebbaad1de5a09abe953b938c70c93
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 00:22:53 +0600

    if no such element founc, printing an error message

--- practise.py
+++ practise.py
@@ -246,3 +246,5 @@ browser.get('https://inventwithpython.com/')
 try:
     elem = browser.find_element_by_class_name(' cover-thumb')
     print('Found <%s> element with that class name!' % (elem.tag_name))
+except:
+    print('Was not able to find an element with that name')

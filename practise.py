df6713cc6c44b72aa972907b1e4c7a030c20f6fa
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 16:12:48 +0600

    loop over the Responsive object's iter_content() method

--- practise.py
+++ practise.py
@@ -176,4 +176,5 @@ res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 res.raise_for_status()
 
 with open('RomeoAndJuliet.txt', 'wb') as playFile:
-    
+    for chunk in res.iter_content(100000):
+        playFile.write(chunk)

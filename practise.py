55688bb0f5b18202a132ace9ab47e342b4f11f5e
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 18:21:14 +0600

    storing a list of tag objects in elems variable

--- practise.py
+++ practise.py
@@ -196,3 +196,4 @@ import requests, bs4
 exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
 print(type(exampleSoup))
+elems = exampleSoup.select('#author')

7fb58735fcfceb2b24a581757779ff92c92f749d
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 10:34:34 +0600

    tried elements instead of element

--- practise.py
+++ practise.py
@@ -244,7 +244,7 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('https://inventwithpython.com/')
 try:
-    elem = browser.find_element_by_class_name('row')
+    elem = browser.find_elements_by_class_name('row')
     print('Found <%s> element with that class name!' % (elem.tag_name))
 except:
     print('Was not able to find an element with that name')

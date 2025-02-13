a9984f575277cc02e7c10bc958112e382b69515b
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 10:30:19 +0600

    Trying different css selector

--- practise.py
+++ practise.py
@@ -244,7 +244,7 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('https://inventwithpython.com/')
 try:
-    elem = browser.find_element_by_class_name('container')
+    elem = browser.find_element_by_class_name('div.container:nth-child(2) > div:nth-child(1) > div:nth-child(1) > center:nth-child(3) > a:nth-child(2) > img:nth-child(1)')
     print('Found <%s> element with that class name!' % (elem.tag_name))
 except:
     print('Was not able to find an element with that name')

3d740028f92f39cee32169d37451b4817cfc7c7c
ms-siam <mobaroksiam10@gmail.com>
2025-02-14 02:10:38 +0600

    Rewrite the program to submit in leetcode

--- minOper.py
+++ minOper.py
@@ -4,20 +4,22 @@
 num = [2,11,10,1,3]
 k = 10
 
-def minOperations(num, k):
-    if all( i >= k for i in num):
-        return 0
-    operationsCount = 0
-    while len(num) >= 2 and not all( i >= k for i in num):
-        num.sort()
-        x = num[0]
-        y = num[1]
-        del num[0:2]
-        newElem = x * 2 + y
-        num.append(newElem)
-        operationsCount = operationsCount + 1
+class Solution:
+    def minOperations(num, k):
+        if all( i >= k for i in num):
+            return 0
+        operationsCount = 0
+        while len(num) >= 2 and not all( i >= k for i in num):
+            num.sort()
+            x = num[0]
+            y = num[1]
+            del num[0:2]
+            newElem = x * 2 + y
+            num.append(newElem)
+            operationsCount = operationsCount + 1
 
-    if num[-1] < k:
-        return -1
-    return operationsCount
-print(minOperations(num, k))
+        if num[-1] < k:
+            return -1
+        return operationsCount
+Solution().minOperations(nums, k)  
+

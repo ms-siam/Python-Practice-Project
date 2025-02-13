bd85d1aeade35b8fd0513469dbab91d0a6ce7f8e
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 12:14:18 +0600

    getting the password from user and sending it to the keys

--- practise.py
+++ practise.py
@@ -272,4 +272,4 @@ userElem = browser.find_element('id', 'user_name')
 userElem.send_keys(pyip.inputStr('Type your username here'))
 
 passElem = browser.find_element('id', 'user_pass')
-
+passElem.send_keys(pyip.inputPassword('Type your password here'))

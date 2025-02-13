8bb795183b4278d55b1553f3b739d4d4b2ff7b7e
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 10:36:45 +0600

    Tried another class name

--- practise.py
+++ practise.py
@@ -244,7 +244,7 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('https://inventwithpython.com/')
 try:
-    elem = browser.find_elements_by_class_name('row')
+    elem = browser.find_elements_by_class_name('jumbotron')
     print('Found <%s> element with that class name!' % (elem.tag_name))
 except:
     print('Was not able to find an element with that name')

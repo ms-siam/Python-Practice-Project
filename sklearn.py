ed49df551fdc4857f632341009362af703b234d0
ms-siam <mobaroksiam10@gmail.com>
2025-02-17 18:37:39 +0600

    Created a classifier

--- sklearn.py
+++ sklearn.py
@@ -4,3 +4,5 @@ X = np.array([[-1,-1],[-2,-1], [-3,-2], [1,1], [2, 1], [3, 2]])
 #Label Array
 Y = np.array([1,1,1,2,2,2])
 from sklearn.naive_bayes import GaussianNB
+#Create a classifier
+clf = GaussianNB()

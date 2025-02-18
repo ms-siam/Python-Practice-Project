68ac22bbe580a57f6d44f0004d1dae84966e78d6
ms-siam <mobaroksiam10@gmail.com>
2025-02-18 13:11:13 +0600

    assigned row labels to the series using index parameter

--- panDasPractice.py
+++ panDasPractice.py
@@ -4,3 +4,6 @@ print(pd.DataFrame({'Apples': [30], 'Bananas': [21]}, index= ['Box 1']))
 
 #If a DataFrame is a table, a Series is a list
 print(pd.Series([1,2,3,4,5]))
+
+#We can assign row labels to the series using index parameter
+print(pd.Series([30, 40, 45], index=['2015 Sales', '2020 Sales', '2025 Sales'], name='Product A'))

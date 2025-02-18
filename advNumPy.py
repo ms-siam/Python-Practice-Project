3a8eb7ead0e623e1fd7dde2412b9dfb42c402d9d
ms-siam <mobaroksiam10@gmail.com>
2025-02-18 11:07:15 +0600

    Slicing the arr a in a certain point

--- advNumPy.py
+++ advNumPy.py
@@ -1,9 +1,13 @@
 # Broadcasting- 
 import numpy as np
-arr = np.array([1,2,3,4,5,6,7,8,9,0])
+arr = np.array([1,2,3,4,5,6,7,8,9,10])
 #Setting a value with index range(broadcasting)
 arr[0:5] = 10
 print(arr)
 
 arr = np.arange(0,11)
 print(arr)
+
+
+sliceOfArr = arr[6:11]
+
